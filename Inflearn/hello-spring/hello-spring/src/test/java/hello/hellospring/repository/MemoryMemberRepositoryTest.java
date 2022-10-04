70ec4dec7d1cc0153360ff8477b47df75d8e9ec0
roal <replicaset01@gmail.com>
2022-10-05 01:48:20 +0900

    a

---
+++ Inflearn/hello-spring/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,19 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        repository.findById(member.getId());
+    }
+}
